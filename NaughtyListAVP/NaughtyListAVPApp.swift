5a886299b343d91385759b9fb76c19178e6f0c47
Bob Witmer <bwitmer@golden.net>
2025-09-16 11:19:33 -0400

    Initial Commit

---
+++ NaughtyListAVP/NaughtyListAVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaughtyListAVPApp.swift
+//  NaughtyListAVP
+//
+//  Created by Bob Witmer on 2025-09-16.
+//
+
+import SwiftUI
+
+@main
+struct NaughtyListAVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
